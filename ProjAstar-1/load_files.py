315b5418efbad999431678d5378873659ab6eb28
MSU CMPS <CMPS@BO120-iMac-3.local>
2015-11-19 15:11:16 -0600

    addedgraphstuff

--- ProjAstar-1/load_files.py
+++ ProjAstar-1/load_files.py
@@ -4,9 +4,24 @@ Program 5 - Part 1
 """
 import csv
 import json
+import random
+''' Ignore this!!!!
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath: return newpath
+    return None
+'''
 nodes = []
 edges = []
 geometry = {}
+graph = {}
 #obtains all nodes and edges from their respective files
 with open('nodes.csv', 'rb') as csvfile:
     rows = csv.reader(csvfile, delimiter = ',', quotechar = '|')
@@ -28,3 +43,24 @@ print "nodes.csv read containing " + str(len(nodes))
 print "edges.csv read containing " + str(len(edges))
 print "Nodes 203982 contains " + str(len(geometry[str(203982)])) + " points. The geometry follows:"
 print geometry[str(203982)]
+
+''' ignore this!!!!
+for e in edges:
+    A,B = e
+    if A in graph:
+        graph[A].append(B)
+    else:
+        graph[A] = [B]
+boo = True
+iterated = 0
+while(boo):
+    start = random.choice(graph.keys())
+    end = random.choice(graph.keys())
+    print start
+    print end
+    print find_path(graph,start,end)
+    iterated += 1
+    if(find_path(graph,start,end) != None):
+        boo = False
+        print iterated
+'''
